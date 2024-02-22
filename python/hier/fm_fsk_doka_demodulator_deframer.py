38a37597296bd4c3676bf3d54d8833878e936cb7
Egor UB1QBJ <61653684+Foxiks@users.noreply.github.com>
2024-02-22 13:15:12 +0300

    Update fm_fsk_doka_demodulator_deframer.py

--- python/hier/fm_fsk_doka_demodulator_deframer.py
+++ python/hier/fm_fsk_doka_demodulator_deframer.py
@@ -16,9 +16,9 @@ import satellites.hier
 
 
 class fm_fsk_doka_demodulator_deframer (gr.hier_block2):
-    def __init__(self, pack_len=132, samp_rate=48000, syncword="1100010010110010001010011000010101110000011011111011110001011010", threshold=10):
+    def __init__(self, baud_rate=4800, pack_len=132, samp_rate=48000, syncword="1100010010110010001010011000010101110000011011111011110001011010", taps=16, threshold=10):
         gr.hier_block2.__init__(
-            self, "FM-FSK (DOKA) Demodulator and Deframer",
+            self, "FM-FSK (DOKA) Demodulator+Deframer",
                 gr.io_signature(1, 1, gr.sizeof_float*1),
                 gr.io_signature(1, 1, gr.sizeof_char*1),
         )
@@ -26,9 +26,11 @@ class fm_fsk_doka_demodulator_deframer (gr.hier_block2):
         ##################################################
         # Parameters
         ##################################################
+        self.baud_rate = baud_rate
         self.pack_len = pack_len
         self.samp_rate = samp_rate
         self.syncword = syncword
+        self.taps = taps
         self.threshold = threshold
 
         ##################################################
@@ -41,7 +43,7 @@ class fm_fsk_doka_demodulator_deframer (gr.hier_block2):
             threshold=threshold,
         )
         self.pdu_pdu_to_tagged_stream_0_0 = pdu.pdu_to_tagged_stream(gr.types.byte_t, 'packet_len')
-        self.freq_xlating_fir_filter_xxx_1 = filter.freq_xlating_fir_filter_fcc(10, [16], 0, samp_rate)
+        self.freq_xlating_fir_filter_xxx_1 = filter.freq_xlating_fir_filter_fcc((int(samp_rate/baud_rate)), [taps], 0, samp_rate)
         self.blocks_pack_k_bits_bb_0 = blocks.pack_k_bits_bb(1)
         self.blocks_float_to_uchar_1 = blocks.float_to_uchar()
         self.analog_rail_ff_1 = analog.rail_ff(0, 1)
@@ -60,6 +62,12 @@ class fm_fsk_doka_demodulator_deframer (gr.hier_block2):
         self.connect((self, 0), (self.freq_xlating_fir_filter_xxx_1, 0))
         self.connect((self.pdu_pdu_to_tagged_stream_0_0, 0), (self, 0))
 
+
+    def get_baud_rate(self):
+        return self.baud_rate
+
+    def set_baud_rate(self, baud_rate):
+        self.baud_rate = baud_rate
 
     def get_pack_len(self):
         return self.pack_len
@@ -80,6 +88,13 @@ class fm_fsk_doka_demodulator_deframer (gr.hier_block2):
     def set_syncword(self, syncword):
         self.syncword = syncword
         self.satellites_sync_to_pdu_packed_0.set_sync(self.syncword)
+
+    def get_taps(self):
+        return self.taps
+
+    def set_taps(self, taps):
+        self.taps = taps
+        self.freq_xlating_fir_filter_xxx_1.set_taps([self.taps])
 
     def get_threshold(self):
         return self.threshold
