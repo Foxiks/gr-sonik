e718397f82e1581819592130a0ed444b1c3a6412
Egor UB1QBJ <61653684+Foxiks@users.noreply.github.com>
2024-02-26 16:47:59 +0300

    [New block] Soniks Decoded Frames Sink

--- python/hier/__init__.py
+++ python/hier/__init__.py
@@ -8,13 +8,8 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
 
-'''
-gr-satellites hierarchical flowgraphs
-
-These are Python files compiled from hierarchical flowgraphs
-'''
 
 from .float32IQ_to_complex import float32IQ_to_complex
 from .complex_to_float32IQ import complex_to_float32IQ
 from .fm_fsk_doka_demodulator_deframer import fm_fsk_doka_demodulator_deframer
-
+from .soniks_data_sink import soniks_data_sink
---
+++ python/hier/soniks_data_sink.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+# GNU Radio Python Flow Graph
+# Title: Soniks Data Sink
+# GNU Radio version: 3.10.7.0
+
+import numpy
+from gnuradio import gr
+import datetime
+import time
+
+class soniks_data_sink(gr.sync_block):
+    """
+    Soniks Network Data Sink
+    """
+    def __init__(self,decoded_data_file_path="/tmp/.satnogs/data/data", sat_id="0",packet_len=132):
+        gr.sync_block.__init__(self,
+            name="Soniks Network Frames Sink",
+            in_sig=[numpy.ubyte],
+            out_sig=[numpy.ubyte])
+        
+        self.decoded_data_file_path=decoded_data_file_path
+        self.sat_id=sat_id
+        self.packet_len=packet_len
+        self.x=0
+        self.pre_x=0
+        self.old_pre_x=0
+        self.out_frame_temp_arr=[]
+
+    def work(self, input_items, output_items):
+        in0 = input_items[0]
+        out = output_items[0]
+        # <+signal processing here+>
+        for i in range(len(in0)):
+            self.out_frame_temp_arr.append(in0[i])
+        if(len(self.out_frame_temp_arr)==self.packet_len):
+            self.pre_x=time.mktime(datetime.datetime.now().timetuple())
+            if(self.pre_x>self.old_pre_x):
+                self.x=0
+            else:
+                self.x+=1
+            time_st=datetime.datetime.now().strftime(f"%Y-%m-%dT%H-%M-%S")
+            with open(f"{self.decoded_data_file_path}_{self.sat_id}_{time_st}_g{self.x}",'wb') as out1:
+                out1.write(int(''.join([hex(self.out_frame_temp_arr[i])[2:].zfill(2) for i in range(len(self.out_frame_temp_arr))]), 16).to_bytes(self.packet_len,'big'))
+            self.out_frame_temp_arr.clear()
+            self.old_pre_x=self.pre_x
+        elif(len(self.out_frame_temp_arr)>self.packet_len):
+            self.pre_x=time.mktime(datetime.datetime.now().timetuple())
+            if(self.pre_x>self.old_pre_x):
+                self.x=0
+            else:
+                self.x+=1
+            time_st=datetime.datetime.now().strftime(f"%Y-%m-%dT%H-%M-%S")
+            with open(f"{self.decoded_data_file_path}_{self.sat_id}_{time_st}_g{self.x}",'wb') as out1:
+                out1.write(int(''.join([hex(self.out_frame_temp_arr[i])[2:].zfill(2) for i in range(self.packet_len)]), 16).to_bytes(self.packet_len,'big'))
+            self.out_frame_temp_arr=self.out_frame_temp_arr[self.packet_len:]
+            self.old_pre_x=self.pre_x
+        else:
+            pass
+        # <+ --- +>
+        
+        out[:] = in0
+        return len(output_items[0])
